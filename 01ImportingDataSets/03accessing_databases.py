f18ede80d0f441ea8e47d4c43bc3318183eb2713
laiananardi <lailai.santiago@gmail.com>
2021-07-14 06:21:40 -0300

    accessing databases

---
+++ 01ImportingDataSets/03accessing_databases.py
@@ -0,0 +1,33 @@
+#Connection Objects
+#   ->database connections
+#   ->manage transactions
+
+#   connection methods
+#       -cursor() method returns a new cursor object using the connection.
+#       -commit() method is used to commit any pending transaction to the 
+#       database.
+#       -rollback() methbod causes the database to roll back to the start of any 
+#       pending transaction.
+#       -close() method is used to close a database connection.
+
+#Cursor Objects
+#   ->database querys
+
+from dbmodule import connect
+
+#create connection object
+connection = connect('datanasename', 'username', 'pswd')
+
+#create a cursor object
+cursor() = connection.cursor()
+
+#run querys
+cursor.execute('select * from mytable')
+results = cursor.fetchall()
+
+#free resources
+cursor.close()
+connection.close()
+
+
+
