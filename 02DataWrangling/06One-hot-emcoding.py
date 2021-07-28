71975a36adfb22f458ac270034733eab163708eb
laiananardi <lailai.santiago@gmail.com>
2021-07-28 06:08:20 -0300

    dummies method

---
+++ 02DataWrangling/06One-hot-emcoding.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+other_path = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/Data%20files/auto.csv"
+df = pd.read_csv(other_path, header=None)
+
+headers = ["symboling","normalized-losses","make","fuel-type","aspiration", "num-of-doors","body-style",
+         "drive-wheels","engine-location","wheel-base", "length","width","height","curb-weight","engine-type",
+         "num-of-cylinders", "engine-size","fuel-system","bore","stroke","compression-ratio","horsepower",
+         "peak-rpm","city-mpg","highway-mpg","price"]
+
+df.columns = headers
+
+print(df["fuel-type"])
+print(pd.get_dummies(df["fuel-type"]))
